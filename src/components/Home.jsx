432e59b290f6a1a480fc09a4928beb3c3db2de75
Arohan Dutt <arohandutt@gmail.com>
2019-11-16 16:54:21 -0600

    Break code for exercise

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,41 @@
 import React, {Component} from 'react';
 
+const dummyData = [
+    {
+        name: 'elephant',
+        features: ['trunk', 'tail']
+    },
+    {
+        name: 'lion',
+        features: ['mane', 'roar']
+    },
+    {
+        name: 'snake',
+        features: ['venom', 'bite']
+    }
+]
+
 class Home extends Component {
     constructor(props) {
         super(props)
-        this.state = {};
+        this.state = {
+            data: dummyData
+        };
     }
     render() {
+        console.log(this.state.data)
         return (
-            <div>You are on the home page</div>
+           <div>
+               {
+                   data.map((animal, key) => {
+                       return <div key={key} onClick={() => someFunc(key)}>I am an {animal.name} and I have a 
+                           {
+                               animal.features.map((feature, key) => <span key={key}>{' ' + feature + ', '}</span>)
+                           }
+                           </div>
+                   })
+               }
+           </div>
         )
     }
 }
